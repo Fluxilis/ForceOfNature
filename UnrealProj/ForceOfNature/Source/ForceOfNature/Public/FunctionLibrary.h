cc485dc97b5c4e438d0dc0701f537803bbd04cba
Fluxilis <felixschoeller95@gmail.com>
2018-06-25 19:44:06 +0200

    -can now load textures by path / filename -more progress on disaster quiz

---
+++ UnrealProj/ForceOfNature/Source/ForceOfNature/Public/FunctionLibrary.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORCEOFNATURE_API UFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Texture By Name")
+		static UTexture2D * GetTextureByName(const FString& Path, const FString& TextureName);
+	UFUNCTION(BlueprintCallable, Category = "Texture By Name")
+		static UTexture2D * GetTextureByPath(const FString& Path);
+	
+	
+};
